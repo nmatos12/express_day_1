24ad5bf6e347c95f25920aa941e08dfbce999f17
unknown <nataliematos12@gmail.com>
2023-04-04 21:14:44 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const terms = require('')
+const PORT = 3000;
+
+// Not supported in ES6
+// const dir = path.join(__dirname);
+
+// console.log(dir);
+
+const app = express();
+
+app.use(express.static('public_files'));
+
+app.get('/', (client_request_obj, server_response_obj) => {
+    // server_response_obj.send('Hello there');
+    const pathToIndex = path.join(process.cwd(), 'views/index.html');
+    server_response_obj.sendFile(pathToIndex);
+});
+
+app.get('/api', (client_request_obj, server_response_obj) => {
+    const params = client_request_obj.query;
+    console.log(params);
+    if (params.term) {
+        terms = terms.filter((obj => obj.term === params.term))
+    }
+    server_response_obj.json(terms)
+});
+
+app.listen(PORT, () => {
+    console.log('Server started on %s', PORT);
+});
+
+// app.listen(5000, () => {
+//     console.log('Server started!');
+// });
